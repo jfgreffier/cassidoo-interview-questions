87d7294f77d867a6c16f295e8b5c3dafef666d03
jfgreffier <jf.greffier@gmail.com>
2021-05-03 17:49:20 +0200

    194

---
+++ 194.spec.js
@@ -0,0 +1,10 @@
+const a = (n) => n | 0;
+
+it('rocks', () => {
+  expect(a(1.7)).toBe(1);
+  expect(a(-2.1)).toBe(-2);
+  expect(a(500.4)).toBe(500);
+  expect(a(-369.5)).toBe(-369);
+  expect(a(150)).toBe(150);
+  expect(a(-350)).toBe(-350);
+});
