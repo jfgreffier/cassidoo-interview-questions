34109a08dcb7ef84be49c8059f0c1747888531fc
jfgreffier <jfgreffier@users.noreply.github.com>
2024-03-29 18:28:11 +0100

    refacto: move files

--- src/189.js
+++ src/189.js
@@ -3,8 +3,8 @@ function isValidNumber(string) {
 }
 
 ['7', '0011', '+3.14', '4.', '-.9', '-123.456', '-0.1'].forEach((string) =>
-  console.log(string, isValidNumber(string))
+  console.log(string, isValidNumber(string)),
 );
 ['abc', '1a', 'e8', '–6', '-+3', '95x54e53.'].forEach((string) =>
-  console.log(string, isValidNumber(string))
+  console.log(string, isValidNumber(string)),
 );
